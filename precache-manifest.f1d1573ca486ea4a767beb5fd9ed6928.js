b7a735f4bdded8f7df381cce1d0d25a68b70193e
Shantanu Jaiswal <shantanujaiswal.mantralabsglobal.com>
2020-02-28 03:14:13 +0530

    Updates

--- precache-manifest.f1d1573ca486ea4a767beb5fd9ed6928.js
+++ precache-manifest.f1d1573ca486ea4a767beb5fd9ed6928.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/urlshortner/static/js/runtime~main.ff3437f3.js"
   },
   {
-    "revision": "aaeca4511a82492a9f21",
-    "url": "/urlshortner/static/js/main.9c30b1ae.chunk.js"
+    "revision": "72816c1ff0e1863daeaa",
+    "url": "/urlshortner/static/js/main.2d76a780.chunk.js"
   },
   {
     "revision": "0d7c695003852ae12df6",
     "url": "/urlshortner/static/js/2.b5a1f16b.chunk.js"
   },
   {
-    "revision": "aaeca4511a82492a9f21",
+    "revision": "72816c1ff0e1863daeaa",
     "url": "/urlshortner/static/css/main.61665ea0.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/urlshortner/static/css/2.5e37da77.chunk.css"
   },
   {
-    "revision": "bf25da6a2e918d9abbf333752e6772c0",
+    "revision": "c927c4457624b539a633c89242991914",
     "url": "/urlshortner/index.html"
   }
 ];
