c42c56f680fbbf24928c7dac6d1ca6afb2c65287
Shantanu Jaiswal <shantanujaiswal.mantralabsglobal.com>
2020-02-28 02:34:46 +0530

    Updates

---
+++ precache-manifest.b0484a21ea614cba7ad904a237b7e373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "353c82f2c570c0021603",
+    "url": "/urlshortner/static/js/runtime~main.ff3437f3.js"
+  },
+  {
+    "revision": "cd5295e4addd45ba7512",
+    "url": "/urlshortner/static/js/main.0be72c62.chunk.js"
+  },
+  {
+    "revision": "0d7c695003852ae12df6",
+    "url": "/urlshortner/static/js/2.b5a1f16b.chunk.js"
+  },
+  {
+    "revision": "cd5295e4addd45ba7512",
+    "url": "/urlshortner/static/css/main.61665ea0.chunk.css"
+  },
+  {
+    "revision": "0d7c695003852ae12df6",
+    "url": "/urlshortner/static/css/2.5e37da77.chunk.css"
+  },
+  {
+    "revision": "52816a30eed8151d123313f80394bc32",
+    "url": "/urlshortner/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Leus/precache-manifest.5ee2abe15ef33ae68593e78dcc49948a.js"
+  "/urlshortner/precache-manifest.b0484a21ea614cba7ad904a237b7e373.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Leus/index.html", {
+workbox.routing.registerNavigationRoute("/urlshortner/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
